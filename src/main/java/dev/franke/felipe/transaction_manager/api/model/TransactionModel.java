1e6374e4ca13921825f2b6139af67d70f23c1413
FelipeFranke5 <frankefelipee@gmail.com>
2025-03-28 11:19:22 -0300

    refactor: modify code organization

--- src/main/java/dev/franke/felipe/transaction_manager/api/model/TransactionModel.java
+++ src/main/java/dev/franke/felipe/transaction_manager/api/model/TransactionModel.java
@@ -11,59 +11,27 @@ import java.util.UUID;
 @Table(name = "acquirer_transactions")
 public class TransactionModel {
 
-    public static CieloResponseDTO toCieloResponse(TransactionModel transaction) {
-        return new CieloResponseDTO(
-                transaction.getOrderId(),
-                new CieloResponsePaymentDTO(
-                        transaction.getAcquirerTransactionId(),
-                        transaction.getPaymentType(),
-                        transaction.getPaymentId(),
-                        transaction.getPaymentStatus()));
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
-    public static class TransactionModelBuilder {
+    @Column(name = "order_id")
+    private String orderId;
 
-        private String orderId;
-        private String acquirerTransactionId;
-        private String paymentType;
-        private String paymentId;
-        private Integer paymentStatus;
-        private OffsetDateTime createdAt;
+    @Column(name = "acquirer_transaction_id")
+    private String acquirerTransactionId;
 
-        public TransactionModelBuilder orderId(String orderId) {
-            this.orderId = orderId;
-            return this;
-        }
+    @Column(name = "payment_type")
+    private String paymentType;
 
-        public TransactionModelBuilder acquirerTransactionId(String acquirerTransactionId) {
-            this.acquirerTransactionId = acquirerTransactionId;
-            return this;
-        }
+    @Column(name = "payment_id")
+    private String paymentId;
 
-        public TransactionModelBuilder paymentType(String paymentType) {
-            this.paymentType = paymentType;
-            return this;
-        }
+    @Column(name = "payment_status")
+    private Integer paymentStatus;
 
-        public TransactionModelBuilder paymentId(String paymentId) {
-            this.paymentId = paymentId;
-            return this;
-        }
-
-        public TransactionModelBuilder paymentStatus(Integer paymentStatus) {
-            this.paymentStatus = paymentStatus;
-            return this;
-        }
-
-        public TransactionModelBuilder createdAt(OffsetDateTime createdAt) {
-            this.createdAt = createdAt;
-            return this;
-        }
-
-        public TransactionModel build() {
-            return new TransactionModel(this);
-        }
-    }
+    @Column(name = "created_at")
+    private OffsetDateTime createdAt;
 
     public TransactionModel() {}
 
@@ -92,28 +60,6 @@ public class TransactionModel {
         this.paymentStatus = paymentStatus;
         this.createdAt = createdAt;
     }
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
-
-    @Column(name = "order_id")
-    private String orderId;
-
-    @Column(name = "acquirer_transaction_id")
-    private String acquirerTransactionId;
-
-    @Column(name = "payment_type")
-    private String paymentType;
-
-    @Column(name = "payment_id")
-    private String paymentId;
-
-    @Column(name = "payment_status")
-    private Integer paymentStatus;
-
-    @Column(name = "created_at")
-    private OffsetDateTime createdAt;
 
     public UUID getId() {
         return id;
@@ -171,6 +117,16 @@ public class TransactionModel {
         this.createdAt = createdAt;
     }
 
+    public static CieloResponseDTO toCieloResponse(TransactionModel transaction) {
+        return new CieloResponseDTO(
+                transaction.getOrderId(),
+                new CieloResponsePaymentDTO(
+                        transaction.getAcquirerTransactionId(),
+                        transaction.getPaymentType(),
+                        transaction.getPaymentId(),
+                        transaction.getPaymentStatus()));
+    }
+
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof TransactionModel that)) return false;
@@ -205,5 +161,49 @@ public class TransactionModel {
                 + paymentId + '\'' + ", paymentStatus="
                 + paymentStatus + ", createdAt="
                 + createdAt + '}';
+    }
+
+    public static class TransactionModelBuilder {
+
+        private String orderId;
+        private String acquirerTransactionId;
+        private String paymentType;
+        private String paymentId;
+        private Integer paymentStatus;
+        private OffsetDateTime createdAt;
+
+        public TransactionModelBuilder orderId(String orderId) {
+            this.orderId = orderId;
+            return this;
+        }
+
+        public TransactionModelBuilder acquirerTransactionId(String acquirerTransactionId) {
+            this.acquirerTransactionId = acquirerTransactionId;
+            return this;
+        }
+
+        public TransactionModelBuilder paymentType(String paymentType) {
+            this.paymentType = paymentType;
+            return this;
+        }
+
+        public TransactionModelBuilder paymentId(String paymentId) {
+            this.paymentId = paymentId;
+            return this;
+        }
+
+        public TransactionModelBuilder paymentStatus(Integer paymentStatus) {
+            this.paymentStatus = paymentStatus;
+            return this;
+        }
+
+        public TransactionModelBuilder createdAt(OffsetDateTime createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public TransactionModel build() {
+            return new TransactionModel(this);
+        }
     }
 }
